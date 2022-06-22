4a5c9b4ed491c886a853ad74e22ab9ff23b14bda
Carol <105285220+caroolromero@users.noreply.github.com>
2022-06-22 22:49:48 +0200

    globo01

---
+++ 010 - REACT EJERCICIO/globo-react/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <header className="App-header">
+       
+         <div id='main'>
+           <div id="balloon" ></div> 
+        <p id="counter">Haz hecho click 0 veces </p>
+        <button className="botonclick">Haz Click</button>
+
+
+
+       
+        <img src='https://cdn.dribbble.com/users/234969/screenshots/5404808/medallion_burst_animation.gif' className="congratulation"></img>
+      </div>
+         
+        </header>
+      </div>
+  );
+}
+
+export default App;
